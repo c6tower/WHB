c6bb0f90a996500c6bda839782608016a97b12fe
mi <clock2elizabeth@gmail.com>
2019-07-28 13:45:55 +0900

    change for deploy

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const pg = require('pg');
 
 exports.pool = pg.Pool({
-  host: 'env_host',
-  database: 'env_db',
-  user: 'env_user',
+  host: 'process.env.env_host',
+  database: 'process.env.env_db',
+  user: 'process.env.env_user',
   port: 5432,
-  password: 'env_pass',
+  password: 'process.env.env_pass',
 });
