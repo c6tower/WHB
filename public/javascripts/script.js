4ad6238f0d4f51aafae32a5a4d149bb4742ea539
mi <clock2elizabeth@gmail.com>
2020-05-17 22:29:14 +0900

    Update home design

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,9 +3,3 @@ function openMenu() {
   document.getElementById("headMenu").classList.toggle("active-head-menu");
   document.getElementById("headMenuBtn").classList.toggle("opened");
 }
-
-// home
-function openSelect() {
-  document.getElementById("homeWord").classList.add("active-home-word");
-  document.getElementById("selectLv").classList.add("active-select-lv");
-}
